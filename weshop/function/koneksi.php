680250636fe4d9b1cefc3be40ff104426d9c5c46
pendekarburon <moetama7@gmail.com>
2020-01-10 18:48:17 +0700

    first commit

---
+++ weshop/function/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "weshop";
+
+    $koneksi = mysqli_connect($server, $username, $password, $database) or die ("Koneksi ke database gagal");
